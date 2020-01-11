d2a12bc04f898e22d2933ee6318e25f7cc887bd2
danielorf <danielorf@gmail.com>
2020-01-11 15:00:56 -0800

    Changed license to GPLv3, updated setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='pyhubitat',
+      version='0.0.1',
+      description='A python library for interacting with the Hubitat API',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/danielorf/pyhubitat',
+      author='Daniel Orf',
+      author_email='danielorf@gmail.com',
+      license='GNU GPLv3',
+      packages=find_packages(),
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          "Programming Language :: Python :: 3",
+          "License :: GNU GPLv3",
+          "Operating System :: OS Independent",
+      ],
+      keywords='hubitat',
+      install_requires=['httpx'],
+      python_requires='>=3.6')
