8b7c36a8689d71522d962be2d0d338f1acdeb607
danielorf <danielorf@gmail.com>
2020-01-12 11:53:30 -0800

    Added methods for each endpoint and docstrings

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from setuptools import setup, find_packages
 
-
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
@@ -17,9 +16,9 @@ setup(name='pyhubitat',
       classifiers=[
           'Development Status :: 3 - Alpha',
           "Programming Language :: Python :: 3",
-          "License :: GNU GPLv3",
+          "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
           "Operating System :: OS Independent",
       ],
-      keywords='hubitat',
+      keywords='hubitat home automation',
       install_requires=['httpx'],
       python_requires='>=3.6')
